4e8014ed5e375e93be48464cb1380c91aa3358d7
Emami.114 <emami.114@outlook.de>
2023-01-12 16:34:38 +0100

    Tages 19 Update

--- Tag19_Projekt/Aufgabe7/src/RollTheDice.kt
+++ Tag19_Projekt/Aufgabe7/src/RollTheDice.kt
@@ -1,5 +1,25 @@
 import kotlin.random.Random.Default.nextInt
 
 fun main() {
+    while (true) {
+
+        val spieler1 = (1..7).random()
+        println("Spieler1 zahl: $spieler1")
+        val spieler2 = (1..7).random()
+        println("Spieler2 zahl: $spieler2")
+        when {
+            spieler1 > spieler2 -> println("Spieler 1 gewinnt Zahl ist: $spieler1")
+            spieler2 > spieler1 -> println("Spieler 2 gewinnt Zahl ist: $spieler2")
+            else -> println("Unentschieden, versuche noch mal")
+
+
+        }
+        println("Möchtest du nochmal wurfen: 'ja' oder 'nein'")
+        val input = readln()
+        if (input != "ja"){
+            break
+        }
+
+    }
 
 }
